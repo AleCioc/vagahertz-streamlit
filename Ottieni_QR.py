c2e5851bf3c1fb46072b0971a16efbc77d061a94
alecioc <alessandro@ciociola.me>
2024-03-16 22:13:47 +0100

    remove key from check entrance

--- Ottieni_QR.py
+++ Ottieni_QR.py
@@ -17,29 +17,8 @@ users_df = read_csv_from_gcs_private_bucket(
 )
 
 
-# user_email = st.text_input("Inserisci il tuo indirizzo email:")
-# user_codice_fiscale = st.text_input("Inserisci il tuo codice fiscale")
-#
-# # button_check = st.button("Clicca per ottenere QR code")
-#
-# if True:
-#     cols = st.columns((1, 1))
-#     user_code = users_df.loc[users_df.codice_fiscale == user_codice_fiscale, "user_code"]
-#     if user_email.lower() in users_df.email.str.lower().values and user_codice_fiscale in users_df.codice_fiscale.values:
-#         user_code = users_df.loc[users_df.codice_fiscale == user_codice_fiscale, "user_code"].values[0]
-#         user_real_name = users_df.loc[users_df.codice_fiscale == user_codice_fiscale, "nome"].values[0]
-#         st.subheader("Ciao {}, ecco il tuo QR code per Non Solo Tekno!".format(user_real_name))
-#         read_user_event_qrcode(
-#             "vagahertz",
-#             "events_access/non-solo-techno_2024-03-16/",
-#             user_code + ".png",
-#             storage_client
-#         )
-#     else:
-#         st.warning("Questa coppia email-codicefiscale non è nel registro!")
-
-nome = st.text_input("Inserisci il tuo indirizzo email:")
-cognome = st.text_input("Inserisci il tuo codice fiscale")
+user_email = st.text_input("Inserisci il tuo indirizzo email:")
+user_codice_fiscale = st.text_input("Inserisci il tuo codice fiscale")
 
 # button_check = st.button("Clicca per ottenere QR code")
 
